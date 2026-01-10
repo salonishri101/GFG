4b646eb0be2a4f99ac847005a906a9fbf64d12e6
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-01-10 11:53:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int knapsack(int W, int val[], int wt[]) {
+        
+        int dp[][]= new int[val.length+1][W+1];
+        
+        
+        for(int i =0;i<dp.length;i++){//first col
+            dp[i][0]=0;
+        }
+        
+        for(int j =0;j<dp[0].length;j++){//first row
+            dp[0][j]=0;
+        }
+       
+       
+       
+       for(int i =1;i<dp.length;i++){
+           for(int j =1;j<dp[0].length;j++){
+             
+             //valid
+             if(wt[i-1]<=j){
+                 //include
+                 int ans1= val[i-1]+dp[i-1][j-wt[i-1]];
+                 
+                 //exclude
+                  int ans2= dp[i-1][j];
+                  
+                  dp[i][j]=Math.max(ans1,ans2);
+                 
+             }else{
+                dp[i][j]=dp[i-1][j]; 
+             }
+             
+           }
+       }
+      return dp[val.length][W];  
+    }
+}
