3547a6b50917dfb3e8be77b1aa8d8310d5c5ee51
SALONI KUMARI <Salonikumri1101@gmail.com>
2026-04-25 12:48:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.java
@@ -0,0 +1,13 @@
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        // code here
+        
+        
+     if((n&(1<<k))!=0)   {
+         return true;
+     }
+     
+     return false;
+        
+    }
+}
